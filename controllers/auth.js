ebe0886025614db490b81fe4baa380ffe980fe0c
Adrian Berenguel <adrianberenguel90@gmail.com>
2023-04-08 21:12:24 +0800

    Created ability to send email on sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,56 @@
 const User = require('../models/user') // import the user model
+const jwt = require('jsonwebtoken')
+const sgMail = require('@sendgrid/mail') // sendgrid
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 exports.signup = (req, res) => {
+  const {name, email, password} = req.body; // shows the data send on terminal
+  const saveNewUser = async () => {
+    try {
+      let findUser = await User.findOne({ email }); // promise is the new way of doing this instead of callback as of express 4.18
+      let newUser = new User({name, email, password}) // instantiate the user model imported above.
+  
+      if (findUser) {
+        return res.status(400).json({
+          error: 'Email is taken'
+        }) // This return statement will hinder the code below from being executed if this condition is true
+      }
 
+      const token = jwt.sign({name, email, password}, process.env.JWT_ACCOUNT_ACTIVATION, {expiresIn: '10m'})
+      const emailData = {
+        from: process.env.EMAIL_FROM,
+        to: email,
+        subject: `Account activation link`,
+        html: `
+          <h1>Please use the following link to activate your account</h1>
+          <p>${process.env.CLIENT_URL}/auth/activate/${token}</p>
+          <hr />
+          <p>This email may contain sensitive information</p>
+          <p>${process.env.CLIENT_URL}</p>
+        `
+      }
+
+      await sgMail.send(emailData)
+
+      //await newUser.save() // promise is the new way of doing this instead of callback as of express 4.18
+
+      //return res.json({ message: 'Signup sucess! Please signin'})
+    
+      // there should always be a return inside async
+      return res.json({
+        message: `Email has been sent to ${email}. Follow the instruction to activate your account`
+      })
+    } catch(error) {
+      console.log('SIGNUP ERROR', error)
+
+      return res.status(400).json({
+        error: error
+      })  
+    }
+  };
+
+  saveNewUser();
 }
 
 /**
@@ -21,5 +70,8 @@ exports.signup = (req, res) => {
  *  - Upon clicking on that url, they will taken to our clint/react app where we will grab the encoded jwt which contains user info
  *  - Then we make a request to backend using our react app so that user is finally saved in the database 
  * Sendgrid is used to send the user an email and encode the jwt
+ *  - We can use .then of ES6 or asnyc await of ES6 - https://www.npmjs.com/package/@sendgrid/mail
+ * 
+ * A rule of thumb is to never combine await and then in a function
  * 
  */
